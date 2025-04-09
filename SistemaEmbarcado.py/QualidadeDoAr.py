f872a063dab36a6903a964e2bc789e8b45322741
BUGU1515 <juliotrindade@colegiopaulodetarso.com.br>
2025-04-09 15:25:27 +0000

    SistemaEmbarcado.py

---
+++ SistemaEmbarcado.py/QualidadeDoAr.py
@@ -0,0 +1,25 @@
+import random
+import time
+
+separador = "---------------------------------------------------------------"
+
+def obter_qualidade():
+  qualidade = random.randint(0, 150)
+  return qualidade
+
+
+def controlar_sensor(qualidade):
+  if qualidade > 50:
+    print(f"Tome cuidado: Qualidade {qualidade:}(IQA) começou a ficar prejudicial.")
+    print(separador)
+  else:
+    print(f"O ar está com qualidade excelente: Qualidade {qualidade:}(IQA) não está prejudicial")
+    print(separador)
+
+def monitorar():
+  while True:
+    qualidade = obter_qualidade()
+    controlar_sensor(qualidade)
+    time.sleep(10)
+
+monitorar()
